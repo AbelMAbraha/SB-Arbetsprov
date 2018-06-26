4aca05530081a6cda75dcf6a3afe1a8ea378be35
AbelMAbraha <AbelMAbraha@hotmail.com>
2018-06-27 00:54:59 +0200

    Add project files.

---
+++ SBArbetsprov/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SBArbetsprov.MvcApplication" Language="C#" %>
---
+++ SBArbetsprov/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SBArbetsprov.Startup))]
+namespace SBArbetsprov
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
